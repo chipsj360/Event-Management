0de0e5bd06322d6a66a14928282c5de4ac590cc9
chipjs360 <josephchipate@gmail.com>
2025-03-22 10:28:57 +0200

    updates

--- event_management/views.py
+++ event_management/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Event, Venue
+
+def index(request):
+    events = Event.objects.all()
+    total_events = events.count()
+    approved_events = events.filter(approval_status='approved').count()
+    available_venues = Venue.objects.count()
+
+    context = {
+        'events': events,
+        'total_events': total_events,
+        'approved_events': approved_events,
+        'available_venues': available_venues,
+    }
+    
+    return render(request, 'index.html', context)
