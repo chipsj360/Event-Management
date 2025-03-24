e9e2b6c953e6249a09a97c82fb0b74ef920f9469
chipjs360 <josephchipate@gmail.com>
2025-03-24 14:00:24 +0200

    set for presentation

--- event_management/admin.py
+++ event_management/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Venue,Event,Food, EventFood,EventAdmin
-
+from django.contrib.auth.models import User
 # Register your models here.
 admin.site.register(Venue)
 admin.site.register(Food)
 admin.site.register(EventFood)
 admin.site.register(Event, EventAdmin)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            'all': ('admin/css/custom_admin.css',)
+        }
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
